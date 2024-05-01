84367ad03ea61a3f3c058a0d145b546aae961adc
Matias <matiasaratano@gmail.com>
2024-05-01 17:49:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './components/Home';
+import Users from './components/Users';
+import Login from './components/Login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/users" element={<Users />} />
+        {/* Agrega más rutas según sea necesario */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Avatar.js
@@ -0,0 +1,4 @@
+import Logo from '../assets/logobdt.png';
+import { Avatar } from '@mui/material';
+
+<Avatar src={Logo} sx={{ m: 1, bgcolor: 'secondary.main' }} />;
