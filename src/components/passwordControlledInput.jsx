0794a52a3eeef2951176b46728ab0de1c53c43b5
eZeKiel9222 <89851595+eZeKiel9222@users.noreply.github.com>
2024-06-19 12:47:03 -0300

    se modifica passwordInput

--- src/components/passwordControlledInput.jsx
+++ src/components/passwordControlledInput.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import { Input, Text, Box } from '@chakra-ui/react';
 
-const PasswordControlledInput = () => {
-  const [value, setValue] = React.useState('');
-  const handleChange = (event) => setValue(event.target.value);
-
+const PasswordControlledInput = ({ value, onChange }) => {
   return (
     <Box w="100%">
       <Text mb="8px" textAlign="left" color={'#6a4fa7'} fontWeight={'bold'}>
-        Constraseña
+        Contraseña:
       </Text>
       <Input
         value={value}
-        onChange={handleChange}
+        onChange={onChange}
         placeholder="Ingrese su contraseña"
         size="md"
         type="password"
         focusBorderColor="#6750A4"
-        errorBorderColor="red.500"
       />
     </Box>
   );
